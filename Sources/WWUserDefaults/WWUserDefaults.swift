03c70e58d4121ab5f6f3ef395b04c0a564e84019
William.Weng <William.Weng@iit.com.hk>
2023-09-04 12:34:32 +0800

    - v1.0.0

--- Sources/WWUserDefaults/WWUserDefaults.swift
+++ Sources/WWUserDefaults/WWUserDefaults.swift
@@ -3,7 +3,7 @@ import UIKit
 // MARK: - [利用「屬性包裝器」做成UserDefaults加強版](https://medium.com/jeremy-xue-s-blog/swift-程式語言-property-wrappers-574e25cdcd44)
 // [=> @WWUserDefault("KEY") static var userDefault: String?](https://ithelp.ithome.com.tw/articles/10223820)
 @propertyWrapper
-public struct WWUserDefault<T> {
+public struct WWUserDefaults<T> {
     
     let key: String
     
